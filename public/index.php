2132ca677cda5ab28637386c70fe45c314231c53
AnOwlbear <AnOwlbear@protonmail.com>
2022-12-06 09:02:47 +0000

    Implementing basic view system

--- public/index.php
+++ public/index.php
@@ -2,4 +2,7 @@
 
 use GroupThr3e\AJExchange\Util\View;
 
+require_once '../vendor/autoload.php';
+
 View::setPath('../src/Views');
+echo View::render('index', ['name' => 'test']);
--- src/Util/View.php
+++ src/Util/View.php
@@ -2,27 +2,45 @@
 
 namespace GroupThr3e\AJExchange\Util;
 
+use InvalidArgumentException;
+
 class View
 {
     protected static string $path;
     protected string $name;
     protected array $data;
 
-    public static function setPath(string $path)
+    public static function setPath(string $path): void
     {
         self::$path = $path;
     }
 
-    protected function __construct(array $data)
+    protected function __construct(string $name, array $data)
     {
+        $this->name = $name;
         $this->data = $data;
     }
 
-    protected function renderInternal()
+    protected function renderInternal(): string
     {
-        $fullPath = self::$path . DIRECTORY_SEPARATOR . $this->name;
+        $fullPath = self::$path . DIRECTORY_SEPARATOR . $this->name . '.phtml';
         if (file_exists($fullPath)) {
+            extract($this->data);
+            ob_start();
             require_once $fullPath;
+            $content = ob_get_clean();
+
+            ob_start();
+            require_once self::$path . DIRECTORY_SEPARATOR . '_layout.phtml';
+            return ob_get_clean();
         }
+
+        throw new InvalidArgumentException();
+    }
+
+    public static function render(string $name, array $data): string
+    {
+        $view = new View($name, $data);
+        return $view->renderInternal();
     }
 }
