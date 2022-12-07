c4805fe187d7042f11d5268b30beca2844d6097f
AnOwlbear <AnOwlbear@protonmail.com>
2022-12-07 23:48:54 +0000

    Updated pdo setup to not use emulated prepares

--- src/Util/DatabaseSingleton.php
+++ src/Util/DatabaseSingleton.php
@@ -13,6 +13,7 @@ class DatabaseSingleton
         if (self::$dbHandle === null) {
             $config = parse_ini_file('../config.ini');
             self::$dbHandle = new PDO($config['dsn'], $config['username'], $config['password']);
+            self::$dbHandle->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         }
 
         return self::$dbHandle;
