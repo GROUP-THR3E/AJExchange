5e43ba6ef232d2d2e66f80b6f923451c7fea4278
4l1onGit <hassanali2562@gmail,com>
2022-12-08 07:47:45 +0000

    Basic frontend design for Search page, Includes css required and Listings Controller

---
+++ src/Controllers/ListingsController.php
@@ -0,0 +1,14 @@
+<?php
+
+use GroupThr3e\AJExchange\Util\View;
+use Slim\App;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+return function(App $app) {
+    $app->get('/listings', function (Request $request, Response $response) {
+        $view = View::render('listings/search', ['name' => 'test']);
+        $response->getBody()->write($view);
+        return $response;
+    });
+};
