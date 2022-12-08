bd4c7047c47de1c7a35894aae493cb0fecf71698
4l1onGit <hassanali2562@gmail,com>
2022-12-08 16:40:25 +0000

    Implemented backend for search page by making changes to listings controller and search.phtml

--- src/Controllers/ListingsController.php
+++ src/Controllers/ListingsController.php
@@ -1,16 +1,22 @@
 <?php
 
+use GroupThr3e\AJExchange\Util\ListingDataset;
 use GroupThr3e\AJExchange\Util\View;
 use Slim\App;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
 return function(App $app) {
+
     $app->get('/listings/search', function (Request $request, Response $response) {
-        $view = View::render('listings/search');
+        $dataset = new ListingDataset();
+        $params = $request->getQueryParams();
+        $listings = $dataset->searchListings($params['query']);
+        $view = View::render('listings/search', ['listings' => $listings]);
         $response->getBody()->write($view);
         return $response;
     });
+
 
     $app->get('/listings', function (Request $request, Response $response) {
         $view = View::render('/listings/view');
