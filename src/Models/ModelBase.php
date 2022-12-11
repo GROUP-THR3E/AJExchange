a39815a1af6811a47eaf9111f9d8066aa41f8750
AnOwlbear <AnOwlbear@protonmail.com>
2022-12-11 00:07:36 +0000

    Added model base class to use for automatically populating model data

---
+++ src/Models/ModelBase.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace GroupThr3e\AJExchange\Models;
+
+use ReflectionClass;
+use ReflectionException;
+
+class ModelBase
+{
+    // This base constructor automatically takes values from the given dbRow and assigns them to the properties of this
+    // class. Only properties of primitive types should be set directly. If the property inherits from ModelBase
+    // then a new instance of it is created using the same dbRow. Because of this, property names have to be unique
+    // between models.
+    public function __construct(array $dbRow)
+    {
+        // Creates a ReflectionClass to check model's properties
+        $classReflector = new ReflectionClass($this);
+        foreach ($classReflector->getProperties() as $property) {
+            $propertyName = $property->getName();
+
+            // Tries creating a ReflectionClass to check if the property inherits ModelBase. If the property is a
+            // primitive type an exception is thrown the dbRow is checked to assign a value directly.
+            try {
+                $propertyReflector = new ReflectionClass($property->getType()->getName());
+                $dbRowCheck = $propertyReflector->getMethod('checkDbRow')->getClosure();
+
+                // Checks the properties class, and only creates a new class if the dbRow contains enough data
+                if ($propertyReflector->isSubclassOf(ModelBase::class) ) {
+                    $this->$propertyName = $dbRowCheck($dbRow)
+                        ? $propertyReflector->newInstanceArgs([$dbRow])
+                        : null;
+                }
+            } catch (ReflectionException $e) {
+                if ($property->getType()->getName() != 'array' && isset($dbRow[$propertyName])) {
+                    $this->$propertyName = $dbRow[$propertyName];
+                }
+            }
+        }
+    }
+
+    // Checks that all the properties that aren't an array or inherit ModelBase are set present in the dbRow
+    protected static function checkDbRow(array $dbRow): bool
+    {
+        $classReflector = new ReflectionClass(self::class);
+        foreach ($classReflector->getProperties() as $property) {
+            // Checks if the property, returning false if it doesn't inherit ModelBase, isn't an array and doesn't
+            // have a matching key in $dbRow
+            $propertyReflector = new ReflectionClass($property->getType()->getName());
+            if (!$propertyReflector->isSubclassOf(ModelBase::class) && $property->getType()->getName() != 'array'
+                && !isset($dbRow[$property->getName()])) return false;
+        }
+
+        return true;
+    }
+}
