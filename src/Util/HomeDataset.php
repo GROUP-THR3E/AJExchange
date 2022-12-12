bdcb083c758cc562f11f8eae2570a4dd01d5f049
AnOwlbear <AnOwlbear@protonmail.com>
2022-12-12 11:40:05 +0000

    Updated home dataset query to join images table

--- src/Util/HomeDataset.php
+++ src/Util/HomeDataset.php
@@ -10,25 +10,31 @@ class HomeDataset extends DatasetBase
 {
     public function getHomepage(): HomepageData
     {
-        $listingQuery = 'SELECT * FROM Listing WHERE type = :type LIMIT 4';
+        $listingQuery =
+            'SELECT * FROM Listing
+             LEFT JOIN ListingImage ON Listing.listingId = ListingImage.listingId
+             WHERE type = :type
+             AND imageIndex = 1
+             LIMIT 4';
+
         $statement = $this->dbHandle->prepare($listingQuery);
 
         $statement->execute(['type' => ListingType::SALE]);
         $saleResults = [];
         foreach ($statement->fetchAll() as $result) {
-            $saleResults[] = new Listing($result);
+            $saleResults[] = new Listing($result, [$result['filename']]);
         }
 
         $statement->execute(['type' => ListingType::EXCHANGE]);
         $exchangeResults = [];
         foreach ($statement->fetchAll() as $result) {
-            $exchangeResults[] = new Listing($result);
+            $exchangeResults[] = new Listing($result, [$result['filename']]);
         }
 
         $statement->execute(['type' => ListingType::GIVEAWAY]);
         $giveawayResults = [];
         foreach ($statement->fetchAll() as $result) {
-            $giveawayResults[] = new Listing($result);
+            $giveawayResults[] = new Listing($result, [$result['filename']]);
         }
 
         return new HomepageData($saleResults, $exchangeResults, $giveawayResults, []);
