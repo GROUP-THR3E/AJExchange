35fbd2ebcfee24f841988ffc025e27b2fbe058f1
4l1onGit <hassanali2562@gmail,com>
2022-12-13 10:53:29 +0000

    Made changes to ListingsController.php to sort missing array key 'type' message.

--- src/Controllers/ListingsController.php
+++ src/Controllers/ListingsController.php
@@ -11,7 +11,7 @@ return function(App $app) {
     $app->get('/listings/search', function (Request $request, Response $response) {
         $dataset = new ListingDataset();
         $params = $request->getQueryParams();
-        $listings = $dataset->searchListings($params['query'] ?? '', [], $params['type']);
+        $listings = $dataset->searchListings($params['query'] ?? '', [], $params['type'] ?? null);
         $view = View::render('listings/search', ['listings' => $listings, 'params' => $params]);
         $response->getBody()->write($view);
         return $response;
